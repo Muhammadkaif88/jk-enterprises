2ff0fe76dadfeb79f061706854e395ef51d0c492
Muhammed Kaif <edukkitofficial@gmail.com>
2026-05-27 17:23:21 +0530

    Use dynamic import for server entrypoint in Vercel to support ES Modules

--- api/index.js
+++ api/index.js
@@ -1,2 +1,9 @@
-import app from "../server/src/index.js";
-export default app;
+let appPromise;
+
+export default async function handler(req, res) {
+  if (!appPromise) {
+    appPromise = import("../server/src/index.js").then(m => m.default);
+  }
+  const app = await appPromise;
+  return app(req, res);
+}
